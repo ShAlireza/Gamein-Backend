931a3d9c67295c85a51659e1c0eaecf5b07a3cba
mgh_2000 <m.agasemzadeh@gmail.com>
2020-05-28 12:29:47 +0430

    add sponsor model

--- apps/homepage/admin.py
+++ apps/homepage/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class StaffAdmin(admin.ModelAdmin):
+    list_display = ('family_name', 'name', 'team', 'role', 'picture', 'linked_in_url')
+
+
+class SponsorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'sponsor_class', 'picture', 'site_url')
+
+
+admin.site.register(Staff, StaffAdmin)
+admin.site.register(Sponsor, SponsorAdmin)
+
+
+
+
--- apps/homepage/models.py
+++ apps/homepage/models.py
@@ -4,13 +4,25 @@ from django.db import models
 
 class Staff(models.Model):
 
-    team = models.CharField(max_length=64)
+    TeamNames = models.TextChoices('Team Name', 'Site Idea others')
+    # TODO: complete all teams' names
+
+    team = models.CharField(max_length=64, choices=TeamNames.choices)
     name = models.CharField(max_length=64)
     family_name = models.CharField(max_length=64)
     picture = models.ImageField(upload_to="staff_pictures")
     role = models.CharField(max_length=20)
-    linked_in_url = models.URLField()
+    linked_in_url = models.URLField('Linked-in')
 
+
+class Sponsor(models.Model):
+
+    SponsorClass = models.TextChoices('Sponsor Class', 'gold silver bronze')
+
+    name = models.CharField(max_length=100)
+    sponsor_class = models.CharField(max_length=100, choices=SponsorClass.choices)
+    picture = models.ImageField(upload_to="sponsor_pictures")
+    site_url = models.URLField('WebSite')
 
 
 
