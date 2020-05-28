0a7fe71b1789c638fe5d25c721cb8f983099a6e6
mgh_2000 <m.agasemzadeh@gmail.com>
2020-05-28 15:05:09 +0430

    Admin models completed

--- apps/homepage/admin.py
+++ apps/homepage/admin.py
@@ -1,34 +1,58 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import *
 
 
+@admin.register(Staff)
 class StaffAdmin(admin.ModelAdmin):
-    list_display = ('family_name', 'name', 'team', 'role', 'picture', 'linked_in_url')
+    list_display = ('last_name', 'name', 'team', 'role', 'picture', 'show_linked_in_url')
+    list_display_links = ('last_name', )
+    list_editable = ('team',)
+    list_filter = ('team', 'role')
+    search_fields = ['last_name', 'name', 'team', 'role']
+
+    def show_linked_in_url(self, obj):
+        return format_html('<a href="%s">%s</a>' % (obj.linked_in_url, obj.name + "'s LinkedIn"))
+    show_linked_in_url.allow_tags = True
+    show_linked_in_url.short_description = 'Linked-In'
 
 
+@admin.register(Sponsor)
 class SponsorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'sponsor_class', 'picture', 'site_url')
+    list_display = ('name', 'sponsor_class', 'picture', 'show_website')
+    list_editable = ('sponsor_class',)
+    def show_website(self, obj):
+        return format_html('<a href="%s">%s</a>' % (obj.site_url, obj.name + "'s Website"))
+    show_website.allow_tags = True
+    show_website.short_description = 'Website'
+    list_filter = ['sponsor_class']
+    search_fields = ['name']
 
 
+@admin.register(Winner)
 class WinnerAdmin(admin.ModelAdmin):
     list_display = ('title', 'award')
+    list_display_links = ('title',)
+    list_editable = ('award', )
+    search_fields = ['title']
 
 
+@admin.register(Quote)
 class QuoteAdmin(admin.ModelAdmin):
     list_display = ('quoter', 'context')
+    search_fields = ['quoter', 'context']
 
 
+@admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     list_display = ('title', 'persian_date', 'date')
+    list_display_links = ('title', )
+    list_editable = ('persian_date', 'date')
+    search_fields = ['title']
 
 
+@admin.register(Statistics)
 class StatisticsAdmin(admin.ModelAdmin):
     list_display = ('title', 'stat', 'icon')
-
-
-admin.site.register(Staff, StaffAdmin)
-admin.site.register(Sponsor, SponsorAdmin)
-admin.site.register(Winner, WinnerAdmin)
-admin.site.register(Quote, QuoteAdmin)
-admin.site.register(Event, EventAdmin)
-admin.site.register(Statistics, StatisticsAdmin)
+    list_editable = ('icon', )
+    search_fields = ['title']
--- apps/homepage/models.py
+++ apps/homepage/models.py
@@ -9,7 +9,7 @@ class Staff(models.Model):
 
     team = models.CharField(max_length=64, choices=TeamNames.choices)
     name = models.CharField(max_length=64)
-    family_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
     picture = models.ImageField(upload_to="staff_pictures")
     role = models.CharField(max_length=20)
     linked_in_url = models.URLField('Linked-in')
