6c953b462e71437a5d4145cba3e12e456ee01123
mgh_2000 <m.agasemzadeh@gmail.com>
2020-05-28 10:02:22 +0430

    add media root and url

--- apps/homepage/models.py
+++ apps/homepage/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Staff(models.Model):
+
+    name = models.CharField(max_length=64)
+    family_name = models.CharField(max_length=64)
+    picture = models.ImageField(upload_to="staff_pictures")
+    birth_date = models.DateField()
+    phone_number = models.CharField(max_length=32)
+    # major = models.CharField(max_length=128)
+
--- gamein_backend/settings.py
+++ gamein_backend/settings.py
@@ -141,3 +141,10 @@ EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = config('EMAIL_PORT')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
