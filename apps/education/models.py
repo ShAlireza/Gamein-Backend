58d0cdd59320cc14c843afc4a3ea33b8b034edec
rezaaminimajd <reza.amini.majd1379@gmail.com>
2020-05-31 00:35:39 +0430

    create models

--- apps/education/models.py
+++ apps/education/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class UserRoleContent(models.Model):
+    title = models.CharField(max_length=64)
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=64)
+    order = models.PositiveSmallIntegerField()
+    document = models.OneToOneField('education.UserRoleContent',
+                                    on_delete=models.CASCADE)
+    is_open = models.BooleanField(default=False)
+    is_read = models.BooleanField(default=False)
+    role = models.ForeignKey('', on_delete=models.CASCADE)
