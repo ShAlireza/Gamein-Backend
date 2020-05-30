cfdf24905b49e281bbfffca660ee014cf65bac85
mhbahmani <mhbahmani79@gmail.com>
2020-05-30 17:25:37 +0430

    add EmailContentAdmin to admin.py

--- apps/emails/admin.py
+++ apps/emails/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.db import models
+from martor.widgets import AdminMartorWidget
 
-# Register your models here.
+from apps.emails.models import EmailContent
+
+
+@admin.register(EmailContent)
+class EmailContentAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': AdminMartorWidget},
+    }
