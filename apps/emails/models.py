7846f1c1edd68ae2add912c60f454ada5d84fe2c
mhbahmani <mhbahmani79@gmail.com>
2020-05-30 15:06:35 +0430

    add EmailContent model

--- apps/emails/models.py
+++ apps/emails/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class EmailContent(models.Model):
+    subject = models.CharField(null=False, max_length=100)
+    text = models.TextField(null=False)
+
+    def __str__(self):
+        return self.subject
