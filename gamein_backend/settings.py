a7ee1c7aa9913e815d31d85158e983beb9482baa
mgh_2000 <m.agasemzadeh@gmail.com>
2020-06-02 01:58:01 +0430

    add postgress db config

--- gamein_backend/settings.py
+++ gamein_backend/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'gamein_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': config('DB_ENGINE', default='django.db.backends.sqlite3'),
+        'NAME': config('DB_NAME', default=os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': config('DB_USER', default=''),
+        'PASSWORD': config('DB_PASSWORD', default=''),
+        'HOST': config('DB_HOST', default=''),
+        'PORT': config('DB_PORT', default=''),
     }
 }
 
