1823b2ce2bf8b69d501826c255cd7c4614bfd6ac
mhbahmani <mhbahmani79@gmail.com>
2020-06-01 20:16:17 +0430

    change save method for Email model

--- apps/emails/models.py
+++ apps/emails/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.template import Context, Template
 from martor.models import MartorField
-from rest_framework.fields import ListField, DictField
 from apps.accounts.models import Profile
 
+import string
 import re
 
 
@@ -18,7 +19,7 @@ class EmailTemplate(models.Model):
              update_fields=None):
         fields = self.find_template_variables(self.html)
         for field in fields:
-            EmailTemplateField.objects.create(force_name=field, template=self)
+            EmailTemplateField.objects.create(field_name=field, template=self)
         super().save(force_insert, force_update, using, update_fields)
 
     def find_template_variables(self, template):
@@ -33,9 +34,29 @@ class EmailTemplateField(models.Model):
 
 class Email(models.Model):
     template = models.OneToOneField(to=EmailTemplate, on_delete=models.CASCADE)
-    subject = models.CharField(null=False, max_length=100)
-    content = models.TextField(null=False)
-    recipients = ListField()
+    subject = models.CharField(blank=True, null=True, max_length=100)
+    values_of_fields = models.TextField(blank=False, null=False)
+    html_context = models.TextField()
+    text_context = models.TextField()
 
     def __str__(self):
         return self.subject
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        context = dict()
+        email_values = re.split('\s\$\$\s', self.values_of_fields)
+        template_fields = self.template.emailtemplatefield_set.all().values_list('field_name', flat=True)
+        for (field, value) in zip(template_fields, email_values):
+            context[field] = value
+        if not self.subject:
+            self.subject = context.get('subject')
+        self.set_values_to_template_fields(context)
+        super().save(force_insert, force_update, using, update_fields)
+
+    def set_values_to_template_fields(self, context):
+        self.html_context = self.render_email_templates(self.template.html, context)
+        self.text_context = self.render_email_templates(self.template.text, context)
+
+    def render_email_templates(self, template: string, context: dict):
+        return Template(template).render(Context(context))
