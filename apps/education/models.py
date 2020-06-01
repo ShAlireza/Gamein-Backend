da8f241d0fe319d9fbecbf956b4027dea9da2619
alireza <alirezashateri7@gmail.com>
2020-06-02 01:16:27 +0430

    Add role to user profile

--- apps/education/models.py
+++ apps/education/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 
-class UserRoleContent(models.Model):
+class Role(models.Model):
     title = models.CharField(max_length=64)
 
 
 class Lesson(models.Model):
     name = models.CharField(max_length=64)
     order = models.PositiveSmallIntegerField()
-    document = models.OneToOneField('education.UserRoleContent',
-                                    on_delete=models.CASCADE)
+    # document = models.OneToOneField('',
+    #                                 on_delete=models.CASCADE)
     is_open = models.BooleanField(default=False)
     is_read = models.BooleanField(default=False)
-    role = models.ForeignKey('', on_delete=models.CASCADE)
+    role = models.ForeignKey('education.Role', on_delete=models.CASCADE)
