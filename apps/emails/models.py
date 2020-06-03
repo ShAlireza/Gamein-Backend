1ebbe0d07bca4dcd85a3750471a21ec8479e5101
mhbahmani <mhbahmani79@gmail.com>
2020-06-03 21:17:12 +0430

    add keywords parser to EmailTemplate model

--- apps/emails/models.py
+++ apps/emails/models.py
@@ -9,6 +9,13 @@ import re
 
 
 class EmailTemplate(models.Model):
+    FIELDS_REGEX = '{{\s(\w+)\s}}'
+    KEYWORDS_REGEX = '{{\s(\w+\.\w+)\s}}'
+    KEYWORDS = [
+        'user.name',
+        'user.university',
+    ]
+    
     title = models.CharField(null=False, max_length=50)
     html = MartorField()
     text = models.TextField(null=False)
@@ -20,12 +27,32 @@ class EmailTemplate(models.Model):
              update_fields=None):
         super().save(force_insert, force_update, using, update_fields)
         fields = self.find_template_variables(self.html)
-        for field in fields:
-            EmailTemplateField.objects.create(field_name=field, template=self)
+        keywords = self.find_template_keywords(self.html)
+        [EmailTemplateField.objects.create(field_name=field, template=self) for field in fields]
+        [EmailTemplateKeywords.objects.create(keyword_type=keyword, template=self) for keyword in keywords]
 
     def find_template_variables(self, template):
-        fields = re.findall('{{\s(\w+)\s}}', template)
+        fields = re.findall(self.FIELDS_REGEX, template)
         return fields
+
+    def find_template_keywords(self, template):
+        keywords = re.findall(self.KEYWORDS_REGEX, template)
+        return keywords
+
+
+class EmailTemplateKeywords(models.Model):
+    USER_NAME = 'UN'
+    USER_UNIVERSITY = 'UU'
+    KEYWORDS = [
+        (USER_NAME, 'user.name'),
+        (USER_UNIVERSITY, 'user.university')
+    ]
+    keyword_type = models.CharField(
+        max_length=3,
+        choices=KEYWORDS,
+        default=None,
+    )
+    template = models.ForeignKey(EmailTemplate, on_delete=models.CASCADE)
 
 
 class EmailTemplateField(models.Model):
