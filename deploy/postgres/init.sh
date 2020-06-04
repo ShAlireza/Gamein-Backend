d0239e44ffcf6ea3158b66a9281f6cb5abac394a
alireza <alirezashateri7@gmail.com>
2020-06-04 20:12:39 +0430

    Fix postgers init file

--- deploy/postgres/init.sh
+++ deploy/postgres/init.sh
@@ -1,6 +1,6 @@
 set -e
 
 su - postgres bash -c "
-    createuser -E -w $user << '$pass';
-    createdb $name -O $user;
+    createuser -E -w $DB_USER << '$DB_PASS';
+    createdb $DB_NAME -O $DB_USER;
     "
