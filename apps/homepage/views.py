403f7480ca20cf0180e04b848fec337a058b9e70
mgh_2000 <m.agasemzadeh@gmail.com>
2020-06-05 15:09:54 +0430

    add register count to homepage view

--- apps/homepage/views.py
+++ apps/homepage/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from rest_framework.generics import GenericAPIView
 from .serializers import *
 from rest_framework.response import Response
+from apps.accounts.models import *
 from drf_yasg.utils import swagger_auto_schema
 
 
 class HomepageView(GenericAPIView):
     def get(self, request):
-        # TODO: update stats from other models when created
         data = {
             'about': AboutSerializer(About.objects.all(), many=True).data,
             'statistics': StatisticsSerializer(Statistics.objects.all(), many=True).data,
@@ -16,7 +16,12 @@ class HomepageView(GenericAPIView):
             'staffs': StaffSerializer(Staff.objects.all().order_by('?')[:5], many=True).data,
             'sponsors': SponsorSerializer(Sponsor.objects.all().order_by('sponsor_class'), many=True).data,
             'quotes': QuoteSerializer(Quote.objects.all().order_by('?')[:5], many=True).data,
-            'socials': SocialSerializer(Social.objects.all(), many=True).data
+            'socials': SocialSerializer(Social.objects.all(), many=True).data,
+
+            'register_count': Profile.objects.count(),
+            # TODO: Add Team and pre
+            # 'team_count': Team.objects.count(),
+            # 'pre_register_count': PreReg.objects.count(),
         }
         return Response(data)
 
